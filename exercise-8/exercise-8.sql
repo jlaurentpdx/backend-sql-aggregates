8f65ae3884b9faf5fe07472ad68d8bad72839319
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-03-25 16:16:34 -0700

    8/10 tests passing

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,28 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+
+SELECT
+	name,
+    COUNT(*),
+    RANK() OVER (ORDER BY COUNT(*) DESC) rank
+FROM
+	category
+INNER JOIN
+	film_category
+    ON
+    	category.category_id = film_category.category_id
+INNER JOIN
+	film
+    ON
+    	film_category.film_id = film.film_id
+INNER JOIN
+	inventory
+    ON
+    	film.film_id = inventory.film_id
+INNER JOIN
+	rental
+	ON
+    	rental.inventory_id = inventory.inventory_id
+GROUP BY
+	name
+  
